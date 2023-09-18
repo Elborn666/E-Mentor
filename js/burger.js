6e776f3f83a6ca3eb7b1f5ee9cb2c65ecb86fcb2
Elborn666 <elborn18@gmail.com>
2023-09-18 16:28:35 +0300

    зробив бургер

---
+++ js/burger.js
@@ -0,0 +1,29 @@
+const openBurgerBtn = document.querySelector('.header__burger-icon')
+const closeBurgerBtn = document.querySelector('.header__burger-cross')
+const menuBurger = document.querySelector('.burger__menu')
+const body = document.querySelector('body')
+const eventsClose = document.querySelectorAll('.burger__link')
+
+openBurgerBtn.addEventListener('click', onOpenBurger)
+closeBurgerBtn.addEventListener('click', onCloseBurger)
+
+function onOpenBurger (){
+    menuBurger.classList.add('active__burger')
+    body.classList.add('hidden')
+    closeBurgerBtn.classList.add('active__burger')
+}
+
+function onCloseBurger (){
+    menuBurger.classList.remove('active__burger')
+    body.classList.remove('hidden')
+    closeBurgerBtn.classList.remove('active__burger')
+}
+
+for (let eventClose of eventsClose) {
+    eventClose.addEventListener('click', onCloseModalBurger);
+  }
+function onCloseModalBurger (){
+    menuBurger.classList.remove('active__burger')
+    body.classList.remove('hidden')
+    closeBurgerBtn.classList.remove('active__burger')
+}
